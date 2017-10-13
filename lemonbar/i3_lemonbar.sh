1e6e2e72ca254ff656b27ba50868336098fdf2c2
Alex Pearson <alex@alexpear.com>
2017-10-12 21:48:36 -0500

    Prune lemonbar configuration

--- lemonbar/i3_lemonbar.sh
+++ lemonbar/i3_lemonbar.sh
@@ -14,48 +14,26 @@ trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
 [ -e "${panel_fifo}" ] && rm "${panel_fifo}"
 mkfifo "${panel_fifo}"
 
-# echo "=== Starting up bar at $(date +%c)" >> bar.log
 
 ### EVENTS METERS
 
-# Window title, "WIN"
-xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}" &
-
 # i3 Workspaces, "WSP"
-$(dirname $0)/i3_workspaces.py > ${panel_fifo} &
-
-# IRC, "IRC"
-# only for init
-# ~/bin/irc_warn &
+# @TODO: fix this for workspaces (currently showing as red bar)
+# $(dirname $0)/i3_workspaces.py > ${panel_fifo} &
 
 # Conky, "SYS"
 conky -c $(dirname $0)/i3_lemonbar_conky > "${panel_fifo}" &
 
 ### UPDATE INTERVAL METERS
 cnt_vol=${upd_vol}
-cnt_mail=${upd_mail}
-cnt_mpd=${upd_mpd}
-cnt_bat=${upd_bat}
 cnt_ssid=${upd_ssid}
 
 while :; do
 
   # Volume, "VOL"
   if [ $((cnt_vol++)) -ge ${upd_vol} ]; then
-    echo "VOL$(alsa-status)" > "${panel_fifo}" &
+    echo "VOL$(pactl list sinks | grep -m 1 "Base Volume" | awk '{print $5}')" > "${panel_fifo}" &
     cnt_vol=0
-  fi
-
-  # MPD
-  if [ $((cnt_mpd++)) -ge ${upd_mpd} ]; then
-    printf "%s%s\n" "MPD" "$(mpc current -f '[[%artist% - ]%title%]|[%file%]' 2>&1 | head -c 70)" > "${panel_fifo}"
-    cnt_mpd=0
-  fi
-
-  # Battey, "BAT"
-  if [ $((cnt_bat++)) -ge ${upd_bat} ]; then
-    echo "$(batstat)" > "${panel_fifo}" &
-    cnt_bat=0
   fi
 
   # SSID, "SID"
@@ -70,9 +48,7 @@ while :; do
 done &
 
 #### LOOP FIFO
-
 cat "${panel_fifo}" | $(dirname $0)/i3_lemonbar_parser.sh \
   | lemonbar -p -f "${font}" -f "${iconfont}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" -u 3 &
 
 wait
-
--- lemonbar/i3_lemonbar_parser.sh
+++ lemonbar/i3_lemonbar_parser.sh
@@ -8,8 +8,6 @@
 
 # min init
 irc_n_high=0
-# title="%{F${color_head} B${color_sec_b2} T3}${sep_right}%{F${color_head} B${color_sec_b2}}%{T2} ${icon_prog} %{F${color_sec_b2} B- T3}${sep_right}%{F- B- T1}"
-title="%{F${color_head} B${color_sec_b2} T2} ${icon_prog} %{T1}"
 
 # parser
 while read -r line ; do
@@ -27,10 +25,6 @@ while read -r line ; do
 
       # cpu
       cpu="%{T2}  ${icon_cpu}  %{T1}${sys_arr[4]}% %{F- B-}"
-      # cpu="%{F${cpu_cback} T3}${sep_left}%{F${cpu_cicon} B${cpu_cback}} %{T2}${icon_cpu}%{F${cpu_cfore} T1} ${sys_arr[4]}%%"
-
-      # mem
-      mem="%{F${cpu_cicon} T3}${sep_l_left} %{T2}${icon_mem}%{F${cpu_cfore} T1} ${sys_arr[5]}"
 
       # wlan
       if [ "${sys_arr[8]}" == "down" ]; then
@@ -57,6 +51,7 @@ while read -r line ; do
       vol_oln=-
       vol_ico=$icon_vol
       vol_txt=${vol_arr[1]}
+
       if [[ ${vol_arr[0]} == "M" ]]; then
         vol_frg=${color_fgdark}
         vol_ico="${icon_mute}  "
@@ -64,60 +59,6 @@ while read -r line ; do
       vol="%{B- F${vol_frg} T2}  ${vol_ico} %{T1} ${vol_txt}%{F- B-}"
       ;;
 
-    GMA*)
-      # Gmail
-      gmail="${line#???}"
-      if [ "${gmail}" != "0" ]; then
-        mail_cback=${color_mail}; mail_cicon=${color_back}; mail_cfore=${color_back}
-      else
-        mail_cback=${color_sec_b1}; mail_cicon=${color_icon}; mail_cfore=${color_fore}
-      fi
-      gmail="%{F${mail_cback}}${sep_left}%{F${mail_cicon} B${mail_cback}} %{T2}${icon_mail}%{F${mail_cfore} T1} ${gmail}"
-      ;;
-
-    MPD*)
-      # Music
-      mpd_arr=(${line#???})
-      if [ -z "${line#???}" ]; then
-        song="none";
-      elif [ "${mpd_arr[1]}" == "error:" ]; then
-        song="mpd off";
-      else
-        song="${line#???}";
-      fi
-      mpd="%{T2}${icon_music} %{F- T1}  ${song}"
-      # echo "Setting music display to ${song}" >> bar.log
-      ;;
-
-    BAT*)
-      # Battery readout:
-      #   [0] = integer part
-      #   [1] = charging status (D(ischarging), C(harging))
-      #   [2] = power level (F(ull), N(ormal), L(ow), C(ritical))
-      bat_arr=(${line#???})
-      bat_icons=($icon_battery)
-      if [[ ${bat_arr[1]} == "C" ]]; then
-        ico="${icon_charging} "
-      else
-        ico="${bat_icons[$(((${bat_arr[0]}*(${#bat_icons[@]}-1))/100))]}"
-      fi
-      bkg=-
-      frg="${color_fglight}"
-      oln="${bkg}"
-
-      if [[ ${bat_arr[2]} == "L" ]]; then
-        oln="${color_warning}"
-        bkg="${color_bgdarkhl}"
-      elif [[ ${bat_arr[2]} == "C" ]]; then
-        oln="${color_critical}"
-        bkg="${color_critical}"
-      elif [[ ${bat_arr[2]} == "F" ]]; then
-        oln="${color_success}"
-        bkg="${color_bgdarkhl}"
-      fi
-
-      batamt="%{+u U${oln} B${bkg}} %{F${frg} T2} ${ico} %{T1}${bat_arr[0]}%  %{-u B-}"
-      ;;
 
     WSP*)
       # I3 Workspaces
@@ -135,17 +76,11 @@ while read -r line ; do
         shift
       done
       ;;
-      
-    WIN*)
-      # window title
-      title=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-      title=" %{F- B- T1} ${title}"
-      ;;
 
     SID*)
       ssid="%{F${color_fgdark} T2}  ${icon_ssid}  %{T1}${line#???}"
       ;;
-      
+
   esac
 
   # And finally, output
