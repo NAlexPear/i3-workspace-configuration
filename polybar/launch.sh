2c6cfd500882c7ce7538fa264622e3e44a1879be
Alex Pearson <alex@alexpear.com>
2018-01-01 14:19:04 -0600

    Move and symlink polybar configuration

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar --reload main &
+  MONITOR=$m polybar --config=$HOME/.config/i3/polybar/config --reload main &
 done
