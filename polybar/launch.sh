d4269057e1b4b20f9f02b0fed3045183f7a37828
Alex Pearson <alex@alexpear.com>
2018-01-01 14:47:20 -0600

    Refactor polybar launch and divvy up bar responsibilities

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+MONITORS=($(xrandr --query | grep " connected " | cut -d" " -f1))
+BARS=("left" "right")
 
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar --config=$HOME/.config/i3/polybar/config --reload main &
+for i in "${!MONITORS[@]}"; do
+  MONITOR="${MONITORS[$i]}" polybar --config=$HOME/.config/i3/polybar/config --reload "${BARS[$i]}" &
 done
